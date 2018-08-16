9c2b7ce75c0143ed63fa57994770522f3039a129
TRANPHUOCHIEP <tranphuochiep1997@gmail.com>
2018-08-16 16:26:24 +0700

    change db url

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const io = require('socket.io').listen(server, {
 });
 
 
-server.listen(PORT, function() {
-  console.log(`Server is listening on port ${PORT}`);
+server.listen(process.env.PORT || PORT_CONFIG, function() {
+  console.log(`Server is listening on port ${process.env.PORT || PORT_CONFIG}`);
 });
 
 // -- Import repository
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  // MONGODB_URL: "mongodb://tranphuochiep1997:conkhimoc123@ds139951.mlab.com:39951/doyouloveme",
+  MONGODB_URL: "mongodb://tranphuochiep1997:conkhimoc123@ds139951.mlab.com:39951/doyouloveme",
   // MONGODB_URL: "mongodb://localhost:27017/doyouloveme",
   // MONGODB_URL: "mongodb://172.16.1.204:27017/doyouloveme",
-  MONGODB_URL: "mongodb://192.168.1.14:27017/doyouloveme",
+  // MONGODB_URL: "mongodb://192.168.1.14:27017/doyouloveme",
   PORT_CONFIG: 5000,
   LIMIT: 3,
   LIMIT_MESSAGE: 20,
